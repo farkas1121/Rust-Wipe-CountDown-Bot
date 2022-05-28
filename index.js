360406ad33c230970524141631c3e018384fa8ad
Farkas <milangamer54@gmail.com>
2022-05-28 12:13:19 +0200

    update

--- index.js
+++ index.js
@@ -29,11 +29,14 @@ function CreateEmbed() {
         .setTitle(config.title)
         .setThumbnail(config.thumbnail)
         .setImage(config.image)
-        .setDescription(config.description)
+        .setDescription(
+            config.description +
+                `\n\nForced wipe will always happen on the first Thursday of the month, which means all of our servers are going to wipe.\nThe next forced wipe is going to be <t:${GetForcedWipe()}:D> <t:${GetForcedWipe()}:R>`
+        )
         .setFooter({ text: config.footer });
     config.servers.forEach((server) => {
         let wipetime = server.timestamp;
-        let currenttime = Math.floor(Date.now() / 1000);
+        const currenttime = Math.floor(Date.now() / 1000);
         let i = 0;
 
         if (server.wipecycle_days.length == 0) {
@@ -50,9 +53,35 @@ function CreateEmbed() {
                 }
             } while (currenttime > wipetime);
         }
-        Embed.addField(server.name, server.description + "\n" + `Next wipe: (<t:${wipetime}:D>) - <t:${wipetime}:R>`, config.inline_fields);
+        if (wipetime > GetForcedWipe()) {
+            wipetime = GetForcedWipe();
+        }
+        Embed.addField(server.name, server.description + "\n" + `Next wipe: <t:${wipetime}:D> <t:${wipetime}:R>`, config.inline_fields);
     });
     return Embed;
+}
+
+function GetForcedWipe() {
+    const date = new Date();
+    const firstthursday = GetFirstThursdayOfMonth(date.getUTCFullYear(), date.getUTCMonth());
+
+    if (date < firstthursday) {
+        return firstthursday / 1000;
+    } else {
+        return GetFirstThursdayOfMonth(date.getUTCFullYear(), date.getUTCMonth() + 1) / 1000;
+    }
+}
+
+function GetFirstThursdayOfMonth(year, month) {
+    let first = new Date(year, month, 1);
+    first.setUTCHours(18);
+
+    if (first.getUTCDay() != 4) {
+        do {
+            first.setUTCDate(first.getUTCDate() + 1);
+        } while (first.getUTCDay() != 4);
+    }
+    return first;
 }
 
 client.login(config.token);
